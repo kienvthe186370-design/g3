ba4e4213dee7464c2673b12d41b44898446363de
anhNQ279 <anhnqhe181014@fpt.edu.vn>
2025-12-03 14:21:40 +0700

    Anhnq (#21)
    * login
    
    * logout
    
    ---------
    
    Co-authored-by: phongdinh04 <phongdhhe181925@fpt.edu.vn>
    Co-authored-by: Quanh <Quanh@10.33.75.229>
    Co-authored-by: chipno39 <datnxhe181631@fpt.edu.vn>

---
+++ src/java/controller/LogoutServlet.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        
+        response.sendRedirect(request.getContextPath() + "/login");
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
